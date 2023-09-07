228667b212e27b105b9e75d2709227771237a389
leiDnedyA <aydendiel@gmail.com>
2023-09-06 20:30:59 -0400

    added route w/ validated path param

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
+words = ["dogs", "cats", "electric", "gamer"]
+
+@app.get("/{index}")
+async def check_word(index:int):
+	if index >= len(words):
+		return "Index out of range :("
+	return words[index]
+
 @app.get("/")
 async def root():
-	return {"message": "Hello world!"}
+	return words[random.randrange(len(words))]
 
